0be713d35dfb4ed626259226404f4f5e755f457c
JoseDom18 <199011605+JoseDom18@users.noreply.github.com>
2025-10-06 15:26:53 -0600

    iniciando 67

---
+++ src/com/leetcode/AddBinary/Solution67.java
@@ -0,0 +1,40 @@
+package com.leetcode.AddBinary;
+
+public class Solution67 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Given two binary strings a and b, return their sum as a binary string.
+//
+//				 
+//
+//				Example 1:
+//
+//				Input: a = "11", b = "1"
+//				Output: "100"
+//				Example 2:
+//
+//				Input: a = "1010", b = "1011"
+//				Output: "10101"
+//				 
+//
+//				Constraints:
+//
+//				1 <= a.length, b.length <= 104
+//				a and b consist only of '0' or '1' characters.
+//				Each string does not contain leading zeros except for the zero itself.
+
+		String a = "11";
+		String b = "1";
+	}
+
+	public static String addBinary(String a, String b) {
+		String ls;
+		
+		a.ch
+		
+		
+		return ls;
+	}
+
+}
