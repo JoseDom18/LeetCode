afa4496dbeaa3cc017e979a1df15db5fcb8c7c11
JoseDom18 <199011605+JoseDom18@users.noreply.github.com>
2025-10-12 22:49:50 -0600

    completado

--- src/com/leetcode/AddBinary/Solution67.java
+++ src/com/leetcode/AddBinary/Solution67.java
@@ -21,48 +21,74 @@ public class Solution67 {
 //				a and b consist only of '0' or '1' characters.
 //				Each string does not contain leading zeros except for the zero itself.
 
-		String a = "11";
-		String b = "1";
+		String a = "1";
+		String b = "0";
+
+		addBinary(a, b);
 	}
 
 	public static String addBinary(String a, String b) {
-		char acarreo = '0'; // se vuelve uno si la suma es 1 + 1
 		String ls = ""; // respuesta
 		String maximo = a; // determina el string de mayor tamaño
 		String minimo = b;
-		String suma;
-		
+
+//		if (a.length() == 1 && b.length() == 1 && a.charAt(0) == '0' && b.charAt(0) == '0') return "0";
+
 		// determinar string mas grande
 		if (minimo.length() > maximo.length()) {
 			maximo = b;
 			minimo = a;
 		}
-		
+
+		// arrays de Strings
+		String[] aB = { maximo, minimo };
+		// arrays de punteros
+		int[] punteros = { maximo.length() - 1, minimo.length() - 1 };
+
 		// crear punteros
 		int punteroMaximo = maximo.length();
-		int punteroMinimo = minimo.length();
 
-		// ciclo
-		for(int i = punteroMaximo - 1; i < punteroMaximo; i++) {
-			for(int j = punteroMinimo -1; j >= 0; j--) {
-				
+		// crear matriz rectangular
+		int[][] arr = new int[5][punteroMaximo + 1];
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				arr[i][j] = 48;
 			}
 		}
 
-//		while (true) {
-//			if(maximo.charAt(punteroMaximo) != minimo.charAt(punteroMinimo)) {
-//				if (maximo.charAt(punteroMaximo) == 1 && minimo.charAt(punteroMinimo) == 1) {
-//					acarreo = '1';
-//				}
-//				ls += "1";
-//				punteroMaximo--;
-//				punteroMinimo--;
-//			} else {
-//				ls += "0";
-//				punteroMaximo--;
-//				punteroMinimo--;
-//			}
-//		}
+		// ingresar cadenas en matriz
+		for (int i = 0; i < 2; i++) {
+			int posicion = arr[0].length - 1;
+			while (punteros[i] >= 0) {
+				arr[i][posicion] = aB[i].charAt(punteros[i]);
+				punteros[i]--;
+				posicion--;
+			}
+		}
+
+		// operaciones
+		for (int i = 0; i <= 2; i += 2) { // loop para compara fila 1 y fila 2; fila 3 y fila 4
+			for (int j = arr[0].length - 1; j >= 0; j--) {
+				if (arr[i][j] == arr[i + 1][j]) {
+					arr[i + 2][j] = 48;
+					if (arr[i][j] == 49) {
+						arr[3][j - 1] = 49;
+					}
+				} else {
+					arr[i + 2][j] = 49;
+				}
+			}
+		}
+
+		// guardar los char en String
+		for (int i = 0; i < arr[0].length; i++) {
+			if (arr[arr.length - 1][0] == 48 && i == 0)
+				continue;
+			char valor = (char) arr[arr.length - 1][i];
+			ls += valor;
+		}
+		
 		return ls;
 	}
 }
